b491205926cc3f31f2a8ca6f17a429df3ad98765
Ram <rampushpagiri@gmail.com>
2025-11-13 20:29:53 -0500

    signup and signin feature added

--- chatbot.py
+++ chatbot.py
@@ -13,13 +13,36 @@ class chatbot:
                             4. press 4 t msg a friend
                             5. press any ther key t exit""")
         if user_input == "1":
-            pass
+            self.signup()
         elif user_input == "2":
-            pass
+            self.signin()
         elif user_input == "3":
             pass
         elif user_input == "4":
             pass
         else:
             exit()
+#meet = chatbot()
+    def signup(self):
+        email = input("enter yur email here ->")
+        passwd = input("setup ypur passwrd ->")
+        self.username = email
+        self.pwd = passwd
+        print("yu have singedup succesfully")
+        print("\n")
+        self.menu()
+    def signin(self):
+        if self.username=='' and self.pwd=='':
+            print("please signup fiest by pressing 1 in the main menu")
+        else:
+            uname = input("enter yur username here ->")
+            pwd1 = input("enter yur passwrd ->")
+            if self.username==uname and self.pwd==pwd1:
+                print("yu have signined in successfully")
+                self.lgdin = True
+                self.menu()
+            else:
+                print("please input crrect creds")
+                print("\n")
+                self.menu()
 meet = chatbot()
