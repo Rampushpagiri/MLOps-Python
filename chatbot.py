58b2364e665edb22d99367fca41a9f8f2d8f18cf
Ram <rampushpagiri@gmail.com>
2025-11-14 19:57:56 -0500

    signup and signin

--- chatbot.py
+++ chatbot.py
@@ -11,29 +11,31 @@ class chatbot:
                             2. press 2 t signin
                             3. press 3 t write a pst
                             4. press 4 t msg a friend
-                            5. press any ther key t exit""")
+                            5. press any ther key t exit
+                             ----> """)
         if user_input == "1":
             self.signup()
         elif user_input == "2":
             self.signin()
         elif user_input == "3":
-            pass
+            self.my_pst()
         elif user_input == "4":
-            pass
+            self.sendmsg()
         else:
             exit()
 #meet = chatbot()
     def signup(self):
         email = input("enter yur email here ->")
-        passwd = input("setup ypur passwrd ->")
+        passwd1 = input("setup ypur passwrd ->")
         self.username = email
-        self.pwd = passwd
+        self.pwd = passwd1
         print("yu have singedup succesfully")
         print("\n")
         self.menu()
     def signin(self):
         if self.username=='' and self.pwd=='':
             print("please signup fiest by pressing 1 in the main menu")
+            self.menu()
         else:
             uname = input("enter yur username here ->")
             pwd1 = input("enter yur passwrd ->")
@@ -45,4 +47,25 @@ class chatbot:
                 print("please input crrect creds")
                 print("\n")
                 self.menu()
+
+    def my_pst(self):
+        if self.lgdin == True:
+            txt = input("enter yur message here")
+            print(f" fllwing cntent has been psed {txt}")
+            self.sendmsg()
+        else:
+            print("Yu need t signin first t pst the msg")
+            print("\n")
+            self.menu()
+    def sendmsg(self):
+        if self.lgdin==True:
+            txt = input("Enter yur msg")
+            frnd = input("whm t send the msg?")
+            print(frnd)
+            print(f"yur msh nas been sent t {frnd}")
+        else:
+            print("Yu need t signin first t pst the msg")
+            print("\n")
+            self.menu()
+
 meet = chatbot()
